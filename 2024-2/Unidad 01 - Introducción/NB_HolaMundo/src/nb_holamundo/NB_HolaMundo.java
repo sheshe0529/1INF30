190ac47f9cffccb8d4198260c7ca8ad51cf44a37
andres-melgar <amelgar@pucp.edu.pe>
2024-08-19 07:40:15 -0500

    ejemplo en Netbeans

---
+++ 2024-2/Unidad 01 - Introducción/NB_HolaMundo/src/nb_holamundo/NB_HolaMundo.java
@@ -0,0 +1,16 @@
+package nb_holamundo;
+
+/**
+ *
+ * @author amelgar
+ */
+public class NB_HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!!!");
+    }
+    
+}
