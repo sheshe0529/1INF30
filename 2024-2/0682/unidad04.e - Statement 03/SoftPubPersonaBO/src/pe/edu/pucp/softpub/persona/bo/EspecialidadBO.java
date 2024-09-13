b2f5a35b6ad88068609c4684294548767c6ab529
andres-melgar <amelgar@pucp.edu.pe>
2024-09-13 07:51:51 -0500

    en clase

---
+++ 2024-2/0682/unidad04.e - Statement 03/SoftPubPersonaBO/src/pe/edu/pucp/softpub/persona/bo/EspecialidadBO.java
@@ -0,0 +1,20 @@
+package pe.edu.pucp.softpub.persona.bo;
+
+import pe.edu.pucp.softpub.persona.dao.EspecialidadDAO;
+import pe.edu.pucp.softpub.persona.daoImpl.EspecialidadDAOImpl;
+import pe.edu.pucp.softpub.persona.model.Especialidad;
+import pe.edu.pucp.softpub.persona.model.Nivel;
+
+public class EspecialidadBO {
+    
+    private EspecialidadDAO especialidadDAO;
+    
+    public EspecialidadBO(){
+        this.especialidadDAO = new EspecialidadDAOImpl();
+    }
+    
+    public Integer insertar(Integer idEspecialidad, String nombre_especialidad, String facultad, Nivel nivel){
+        Especialidad especialidad = new Especialidad(idEspecialidad, nombre_especialidad, facultad, nivel);
+        return this.especialidadDAO.insertar(especialidad);
+    }
+}
