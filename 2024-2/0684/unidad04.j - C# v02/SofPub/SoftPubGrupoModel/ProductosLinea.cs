9273900f25fecb3e9e6ec6444216743c16800335
andres-melgar <amelgar@pucp.edu.pe>
2024-09-28 11:09:20 -0500

    en clase

---
+++ 2024-2/0684/unidad04.j - C# v02/SofPub/SoftPubGrupoModel/ProductosLinea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftPubPublicacionModel;
+
+namespace SoftPubGrupoModel
+{
+    public class ProductosLinea
+    {
+        private Linea linea;
+        private Publicacion publicacion;
+    }
+}
