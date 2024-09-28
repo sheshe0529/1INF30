07942ec7751863bb80fb3e0767fb0202f9e68c15
andres-melgar <amelgar@pucp.edu.pe>
2024-09-28 12:07:20 -0500

    en clase

---
+++ 2024-2/0682/unidad04.j - C# v02/SofPub/SoftPubPersonaDA/DAOImpl/SeccionDAOImpl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SofPubDBManager;
+using SoftPubPersonaDA.DAO;
+
+namespace SoftPubPersonaDA.DAOImpl
+{
+    public class SeccionDAOImpl : DAOImplBase, SeccionDAO
+    {
+    }
+}
---
+++ 2024-2/0682/unidad04.k - C# v03/SofPub/SoftPubGrupoDA/DAOImpl/ProductosLineaDAOImpl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SofPubDBManager;
+using SoftPubGrupoDA.DAO;
+
+namespace SoftPubGrupoDA.DAOImpl
+{
+    public class ProductosLineaDAOImpl : DAOImplBase, ProductosLineaDAO
+    {
+    }
+}
