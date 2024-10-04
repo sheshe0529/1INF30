81b12a02cb338a9e9053a5d4fccac24c3e843346
andres-melgar <amelgar@pucp.edu.pe>
2024-10-04 12:01:44 -0500

    clase

--- 2024-2/0684/unidad06.b_Web_02/SofPub/SoftPubPersonaDA/DAO/EspecialidadDAO.cs
+++ 2024-2/0684/unidad06.b_Web_02/SofPub/SoftPubPersonaDA/DAO/EspecialidadDAO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,6 @@ namespace SoftPubPersonaDA.DAO
 {
     public interface EspecialidadDAO
     {
+        BindingList<object> listarTodos();
     }
 }
--- 2024-2/0684/unidad06.b_Web_02/SofPub/SoftPubPersonaDA/DAOImpl/EspecialidadDAOImpl.cs
+++ 2024-2/0684/unidad06.b_Web_02/SofPub/SoftPubPersonaDA/DAOImpl/EspecialidadDAOImpl.cs
@@ -7,23 +7,34 @@ using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using SofPubDBManager;
 using SoftPubPersonaDA.DAO;
+using SoftPubPersonaModel;
 
 namespace SoftPubPersonaDA.DAOImpl
 {
     public class EspecialidadDAOImpl : DAOImplBase, EspecialidadDAO
     {
-        public EspecialidadDAOImpl() : base("espacialidad")
+        public EspecialidadDAOImpl() : base("especialidad")
         {
         }
 
-        protected override void agregarObjetoALaLista(BindingList<object> lista, MySqlDataReader lector)
+        public BindingList<object> listarTodos()
         {
-            throw new NotImplementedException();
+            return base.listarTodos(null);
         }
 
         protected override string obtenerProyeccionParaSelect()
         {
-            throw new NotImplementedException();
+            return "idEspecialidad, especialidad, facultad, nivel";
         }
+
+        protected override void agregarObjetoALaLista(BindingList<object> lista, MySqlDataReader lector)
+        {
+            Especialidad especialidad = new Especialidad();
+            especialidad.IdEspecialidad = lector.GetInt32("idEspecialidad");
+            especialidad.Nombre_especialidad = lector.GetString("especialidad");
+            especialidad.Facultad = lector.GetString("facultad");
+            especialidad.Nivel =  (Nivel)Enum.Parse(typeof(Nivel), lector.GetString("nivel"));
+            lista.Add(especialidad);
+        }        
     }
 }
