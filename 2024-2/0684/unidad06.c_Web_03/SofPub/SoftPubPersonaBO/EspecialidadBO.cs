e2a0ff550d152340da2c38bdd65d2ddacc4295bf
andres-melgar <amelgar@pucp.edu.pe>
2024-10-07 06:57:27 -0500

    preparación de clase

---
+++ 2024-2/0684/unidad06.c_Web_03/SofPub/SoftPubPersonaBO/EspecialidadBO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftPubPersonaDA.DAO;
+using SoftPubPersonaDA.DAOImpl;
+
+namespace SoftPubPersonaBO
+{    
+    public class EspecialidadBO
+    {
+        private EspecialidadDAO especialidadDAO;
+
+        public EspecialidadBO()
+        {
+            this.especialidadDAO = new EspecialidadDAOImpl();
+        }
+
+        public BindingList<Object> listarTodos()
+        {
+            return this.especialidadDAO.listarTodos();
+        }
+    }
+}
