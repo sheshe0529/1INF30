0d813fee9e89d68998d7099f977c61d7602630c0
andres-melgar <amelgar@pucp.edu.pe>
2024-10-12 21:32:52 -0500

    clase

---
+++ 2024-2/Proyecto DAO SoftPub .NET/SoftPub/SoftPubPublicacionModel/Tipo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftPubPublicacionModel
+{
+    public enum Tipo
+    {
+        LIBRO, ARTICULO_EN_REVISTA, ARTICULO_EN_CONFERENCIA, CAPITULO_DE_LIBRO, EDITORIAL
+    }
+}
